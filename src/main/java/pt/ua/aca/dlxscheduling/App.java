08b6df33577074960844bcfbd202fea00c76050a
Carlos Goncalves <mail@cgoncalves.info>
2011-11-04 02:31:47 +0000

    Initial commit

---
+++ src/main/java/pt/ua/aca/dlxscheduling/App.java
@@ -0,0 +1,13 @@
+package pt.ua.aca.dlxscheduling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
